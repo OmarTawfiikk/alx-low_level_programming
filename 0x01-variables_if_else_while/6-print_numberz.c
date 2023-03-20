2a793c33c1f92237acd3cc7609c179da04407900
omar <omartawfik333@gmail.com>
2023-03-20 09:03:08 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all alphabet except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
