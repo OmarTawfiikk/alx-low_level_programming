af0939fce3f778573ee89b9a7105277eeee85c23
omar <omartawfik333@gmail.com>
2023-03-22 16:31:49 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,16 +27,15 @@ void print_times_table(int n)
 				if (prod <= 99)
 					_putchar(' ');
 
-				if (prod <= 100)
+				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 100) % 10 + 48);
-				}
-				else if (prod <= 99 && prod >= 10)
+				}else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
 				_putchar((prod % 100) + 48);
 			}
-		_putchar('\n');
+	i		_putchar('\n');
 		}
 	}
 }
