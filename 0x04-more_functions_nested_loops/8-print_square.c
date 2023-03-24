7ecc5ad729d733dce804452d53d9d255ff328dff
omar <omartawfik333@gmail.com>
2023-03-24 14:51:00 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,25 +7,23 @@
  */
 
 void print_square(int size)
-
 {
 
 	int x, y;
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < n; x++)
-		{
-			for (y = 0; y < n; y++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
