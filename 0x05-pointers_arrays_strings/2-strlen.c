725ae23238da8c01a6c5d9776c79f2ebe643fd3b
omar <omartawfik333@gmail.com>
2023-03-27 08:15:14 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,12 @@
  * @str: The string to get the lengthof
  * Return: The length of @str
  */
-int _strlen(const char *str)
+int _strlen(char *s)
 
 {
 	size_t length = 0;
 
-	while (*str++)
+	while (*s++)
 		length++;
 	return (length);
 }
