fce57b2adbcfcb8be4d9f2529b85c56b939031dd
omar <omartawfik333@gmail.com>
2023-04-04 18:20:05 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - Entry gate
+ * @s: input val
+ * @to: input val2
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
